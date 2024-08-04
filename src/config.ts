70bf37c4262401d9407aed558dc496a0e3cfb2e2
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-08-04 21:12:56 +1200

    feat: wallet connect support

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { polkadot, kusama, paseo, westend } from "@polkadot-api/descriptors";
 import type { Config } from "@reactive-dot/core";
 import { InjectedWalletAggregator } from "@reactive-dot/core/wallets.js";
+import { WalletConnect } from "@reactive-dot/core/wallets/wallet-connect.js";
 import { getSmProvider } from "polkadot-api/sm-provider";
 import { startFromWorker } from "polkadot-api/smoldot/from-worker";
 
@@ -48,5 +49,25 @@ export const config = {
       ),
     },
   },
-  wallets: [new InjectedWalletAggregator()],
+  wallets: [
+    new InjectedWalletAggregator(),
+    new WalletConnect({
+      projectId: import.meta.env.VITE_APP_WALLET_CONNECT_PROJECT_ID,
+      providerOptions: {
+        metadata: {
+          name: "ĐÓTConsole",
+          description: "Substrate development console.",
+          url: globalThis.origin,
+          icons: ["/logo.png"],
+        },
+      },
+      chainIds: [
+        "polkadot:91b171bb158e2d3848fa23a9f1c25182", // Polkadot
+        "polkadot:b0a8d493285c2df73290dfb7e61f870f", // Kusama
+        // Nova Wallet does not support Paseo
+        // "polkadot:77afd6190f1554ad45fd0d31aee62aac", // Paseo
+        "polkadot:e143f23803ac50e8f6f8e62695d1ce9e", // Westend
+      ],
+    }),
+  ],
 } as const satisfies Config;
