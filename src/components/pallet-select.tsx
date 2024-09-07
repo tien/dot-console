b53982a76a6a0c3c32fdad8f3a4dd78592f187a3
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-09-07 22:36:02 +1200

    feat: keep previously selected pallet

--- src/components/pallet-select.tsx
+++ src/components/pallet-select.tsx
@@ -3,6 +3,7 @@ import type { Pallet } from "../types";
 import { Select } from "./ui";
 import Check from "@w3f/polkadot-icons/solid/Check";
 import ChevronDown from "@w3f/polkadot-icons/solid/ChevronDown";
+import { atom, useAtom } from "jotai";
 import { useState, type ReactNode } from "react";
 import { css } from "styled-system/css";
 
@@ -11,18 +12,30 @@ export type PalletSelectProps = {
   children: (props: { pallet: Pallet; palletSelect: ReactNode }) => ReactNode;
 };
 
+const previouslySelectedPalletAtom = atom<number | undefined>();
+
 export function PalletSelect({ filter, children }: PalletSelectProps) {
   const metadata = useMetadata();
-
   const pallets = metadata.value.pallets.filter(filter);
 
-  const defaultPallet = pallets.at(0);
+  const [previouslySelectedPallet, setPreviouslySelectedPallet] = useAtom(
+    previouslySelectedPalletAtom,
+  );
+
+  const defaultPallet =
+    pallets.find((pallet) => pallet.index === previouslySelectedPallet) ??
+    pallets.at(0);
 
   if (defaultPallet === undefined) {
     throw new Error("Metadata doesn't contain any pallet");
   }
 
-  const [selectedPallet, setSelectedPallet] = useState(defaultPallet.index);
+  const [selectedPallet, _setSelectedPallet] = useState(defaultPallet.index);
+
+  const setSelectedPallet = (index: number) => {
+    _setSelectedPallet(index);
+    setPreviouslySelectedPallet(index);
+  };
 
   const palletItems = pallets
     .map((pallet) => ({ label: pallet.name, value: pallet.index }))
