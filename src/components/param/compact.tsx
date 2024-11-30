d1732d4c231ed02d0a68f17e3e5d2f8e3a5081de
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-11-30 17:06:58 +1300

    refactor: integer input

--- src/components/param/compact.tsx
+++ src/components/param/compact.tsx
@@ -1,6 +1,11 @@
-import { Input } from "../ui/input";
-import type { ParamProps } from "./common";
-import { useEffect, useState } from "react";
+import { Field } from "../ui/field";
+import {
+  INCOMPLETE,
+  INVALID,
+  integerPrimitives,
+  type ParamProps,
+} from "./common";
+import { useEffect, useMemo, useState } from "react";
 
 export type CompactParamProps = ParamProps<number | bigint> & {
   compact: { codec: "compactNumber" | "compactBn" };
@@ -16,27 +21,71 @@ export function CompactParam({
 
   const isBig = compact.codec === "compactBn";
 
+  const parsedValue = useMemo(() => {
+    if (value.trim() === "") {
+      return INCOMPLETE;
+    }
+
+    if (!isBig) {
+      const number = Number(value);
+
+      if (
+        Number.isNaN(number) ||
+        !Number.isFinite(number) ||
+        number < SMALL.min ||
+        number > SMALL.max
+      ) {
+        return INVALID;
+      }
+
+      return number;
+    }
+
+    try {
+      const bn = BigInt(value);
+
+      if (bn < BIG.min || bn > BIG.max) {
+        return INVALID;
+      }
+
+      return bn;
+    } catch {
+      return INVALID;
+    }
+  }, [isBig, value]);
+
   useEffect(
     () => {
-      onChangeValue(isBig ? BigInt(value) : Number(value));
+      onChangeValue(parsedValue);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [value],
+    [parsedValue],
   );
 
   return (
-    <Input
-      type="number"
-      inputMode="numeric"
-      placeholder="Compact"
-      value={value}
-      min={
-        isBig
-          ? "-57896044618658097711785492504343953926634992332820282019728792003956564819968"
-          : -2147483648
-      }
-      max={isBig ? "170141183460469231731687303715884105727" : 4294967295}
-      onChange={(event) => setValue(event.target.value)}
-    />
+    <Field.Root
+      required={parsedValue === INCOMPLETE}
+      invalid={parsedValue === INVALID}
+    >
+      <Field.Input
+        type="number"
+        inputMode="numeric"
+        placeholder="Compact"
+        value={value}
+        min={isBig ? BIG.min.toString() : SMALL.min}
+        max={isBig ? BIG.max.toString() : SMALL.max}
+        onChange={(event) => setValue(event.target.value)}
+      />
+    </Field.Root>
   );
 }
+
+const SMALL = {
+  min: integerPrimitives.i32.min,
+  max: integerPrimitives.u32.max,
+};
+
+const BIG = {
+  min: integerPrimitives.i128.min,
+  max: integerPrimitives.u128.max,
+};
--- src/components/param/primitive.tsx
+++ src/components/param/primitive.tsx
@@ -1,8 +1,19 @@
-import { Input } from "../ui/input";
+import { Field } from "../ui/field";
 import { Switch } from "../ui/switch";
-import type { ParamProps } from "./common";
+import {
+  INCOMPLETE,
+  integerPrimitives,
+  INVALID,
+  type ParamProps,
+} from "./common";
 import type { PrimitiveDecoded } from "@polkadot-api/view-builder";
-import { type ChangeEvent, useEffect, useState } from "react";
+import {
+  type ChangeEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 
 type ComplexPrimitive =
   | "AccountId"
@@ -28,121 +39,138 @@ export function PrimitiveParam({
   defaultValue,
   onChangeValue,
 }: PrimitiveParamProps) {
-  const [value, setValue] = useState(defaultValue?.value.toString() ?? "");
+  const [value, setValue] = useState(
+    (defaultValue?.value.toString() ?? primitive.codec === "bool")
+      ? String(false)
+      : "",
+  );
 
-  const commonProps = {
-    placeholder: primitive.codec,
-    value,
-    onChange: (event: ChangeEvent<HTMLInputElement>) =>
-      setValue(event.target.value),
-  };
+  const commonProps = useMemo(
+    () => ({
+      placeholder: primitive.codec,
+      value,
+      onChange: (event: ChangeEvent<HTMLInputElement>) =>
+        setValue(event.target.value),
+    }),
+    [primitive.codec, value],
+  );
 
-  const commonNumberProps = {
-    ...commonProps,
-    type: "number",
-    inputMode: "numeric",
-  } as const;
+  const commonNumberProps = useCallback(
+    (primitive: keyof typeof integerPrimitives) =>
+      ({
+        ...commonProps,
+        type: "number",
+        inputMode: "numeric",
+        min:
+          typeof integerPrimitives[primitive].min === "number"
+            ? integerPrimitives[primitive].min
+            : String(integerPrimitives[primitive].min),
+        max:
+          typeof integerPrimitives[primitive].max === "number"
+            ? integerPrimitives[primitive].max
+            : String(integerPrimitives[primitive].max),
+      }) as const,
+    [commonProps],
+  );
+
+  const parsedValue = useMemo(() => {
+    if (primitive.codec === "str") {
+      return value;
+    }
+
+    if (value.trim() === "") {
+      return INCOMPLETE;
+    }
+
+    switch (primitive.codec) {
+      case "bool":
+        return Boolean(value);
+      case "char":
+        return value.length === 1 ? value : INVALID;
+      case "u8":
+      case "i8":
+      case "u16":
+      case "i16":
+      case "u32":
+      case "i32": {
+        const number = Number(value);
+
+        if (Number.isNaN(number) || !Number.isFinite(number)) {
+          return INVALID;
+        }
+
+        const { min, max } = commonNumberProps(primitive.codec);
+
+        if (number < Number(min) || number > Number(max)) {
+          return INVALID;
+        }
+
+        return number;
+      }
+      case "u64":
+      case "i64":
+      case "u128":
+      case "i128":
+      case "u256":
+      case "i256": {
+        const bn = BigInt(value);
+
+        const { min, max } = commonNumberProps(primitive.codec);
+
+        if (bn < BigInt(min) || bn > BigInt(max)) {
+          return INVALID;
+        }
+
+        return bn;
+      }
+    }
+  }, [commonNumberProps, primitive.codec, value]);
 
   useEffect(
     () => {
-      switch (primitive.codec) {
-        case "bool":
-          onChangeValue(Boolean(value));
-          break;
-        case "char":
-        case "str":
-          onChangeValue(value);
-          break;
-        case "u8":
-        case "i8":
-        case "u16":
-        case "i16":
-        case "u32":
-        case "i32":
-          onChangeValue(Number(value));
-          break;
-        case "u64":
-        case "i64":
-        case "u128":
-        case "i128":
-        case "u256":
-        case "i256":
-          onChangeValue(BigInt(value));
-      }
+      onChangeValue(parsedValue);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [value],
+    [parsedValue],
   );
 
-  switch (primitive.codec) {
-    case "bool":
-      return (
-        <Switch
-          checked={Boolean(value)}
-          onCheckedChange={(event) => setValue(String(event.checked))}
-        />
-      );
-    case "char":
-      return <Input {...commonProps} maxLength={1} />;
-    case "str":
-      return <Input {...commonProps} />;
-    case "i8":
-      return <Input {...commonNumberProps} min={-128} max={127} />;
-    case "u8":
-      return <Input {...commonNumberProps} min={0} max={255} />;
-    case "i16":
-      return <Input {...commonNumberProps} min={-32768} max={32767} />;
-    case "u16":
-      return <Input {...commonNumberProps} min={0} max={65535} />;
-    case "i32":
-      return (
-        <Input {...commonNumberProps} min={-2147483648} max={2147483647} />
-      );
-    case "u32":
-      return <Input {...commonNumberProps} min={0} max={4294967295} />;
-    case "i64":
-      return (
-        <Input
-          {...commonNumberProps}
-          min="-9223372036854775808"
-          max="9223372036854775807"
-        />
-      );
-    case "u64":
-      return (
-        <Input {...commonNumberProps} min={0} max="18446744073709551615" />
-      );
-    case "i128":
-      return (
-        <Input
-          {...commonNumberProps}
-          min="-170141183460469231731687303715884105728"
-          max="170141183460469231731687303715884105727"
-        />
-      );
-    case "u128":
-      return (
-        <Input
-          {...commonNumberProps}
-          min={0}
-          max="340282366920938463463374607431768211455"
-        />
-      );
-    case "i256":
-      return (
-        <Input
-          {...commonNumberProps}
-          min="-57896044618658097711785492504343953926634992332820282019728792003956564819968"
-          max="57896044618658097711785492504343953926634992332820282019728792003956564819967"
-        />
-      );
-    case "u256":
-      return (
-        <Input
-          {...commonNumberProps}
-          min={0}
-          max="115792089237316195423570985008687907853269984665640564039457584007913129639935"
-        />
-      );
-  }
+  const inputElement = useMemo(() => {
+    switch (primitive.codec) {
+      case "bool":
+        return (
+          <Field.Input asChild>
+            <Switch
+              checked={Boolean(value)}
+              onCheckedChange={(event) => setValue(String(event.checked))}
+            />
+          </Field.Input>
+        );
+      case "char":
+        return <Field.Input {...commonProps} maxLength={1} />;
+      case "str":
+        return <Field.Input {...commonProps} />;
+      case "i8":
+      case "u8":
+      case "i16":
+      case "u16":
+      case "i32":
+      case "u32":
+      case "i64":
+      case "u64":
+      case "i128":
+      case "u128":
+      case "i256":
+      case "u256":
+        return <Field.Input {...commonNumberProps(primitive.codec)} />;
+    }
+  }, [commonNumberProps, commonProps, primitive.codec, value]);
+
+  return (
+    <Field.Root
+      required={parsedValue === INCOMPLETE}
+      invalid={parsedValue === INVALID}
+    >
+      {inputElement}
+    </Field.Root>
+  );
 }
